392e5c48d4f7111205a84d24b37e3730a378eea5
David6299 <dheveed3@gmail.com>
2022-04-24 23:54:20 +0000

    commit

--- Webdriver_Practice(Facebook)/Webdriver_Practice(Facebook)/UnitTest1.cs
+++ Webdriver_Practice(Facebook)/Webdriver_Practice(Facebook)/UnitTest1.cs
@@ -11,14 +11,31 @@ namespace Webdriver_Practice_Facebook_
         [TestMethod]
         public void TestFacebookAndPrintFriendsList()
         {
-            int waitingTime = 2000;
+            int waitingTime = 1500;
             By googleSearchBar = By.Name("q");
-            By googleSearchButton = By.Name("btnK"); 
+            By googleSearchButton = By.Name("btnK");
+            By facebookLink = By.XPath("/html/body/div[7]/div/div[10]/div/div[2]/div[2]/div/div/div[1]/div/div/div/div/div/div[1]/a/h3");
             
             IWebDriver webDriver = new ChromeDriver();
-            webDriver.Manage().Window.Minimize();
+            webDriver.Manage().Window.Maximize();
 
             Thread.Sleep(waitingTime);
+
+            webDriver.Navigate().GoToUrl("https://www.google.com/");
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.FindElement(googleSearchBar).SendKeys("facebook login");
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.FindElement(googleSearchButton).Click();
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.FindElement(facebookLink).Click();
+
+            //webDriver.Quit();
         }
     }
 }
