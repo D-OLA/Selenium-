11dca879506b7271328ccf8430d50fba7d7630f7
David6299 <dheveed3@gmail.com>
2022-04-24 23:38:43 +0000

    commit

---
+++ Webdriver_Practice(Facebook)/Webdriver_Practice(Facebook)/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Webdriver_Practice(Facebook)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Webdriver_Practice(Facebook)")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("28062941-f3b3-4ead-afe9-49d1b581fd31")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Webdriver_Practice(Facebook)/Webdriver_Practice(Facebook)/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace Webdriver_Practice_Facebook_
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestFacebookAndPrintFriendsList()
+        {
+            int waitingTime = 2000;
+            By googleSearchBar = By.Name("q");
+            By googleSearchButton = By.Name("btnK"); 
+            
+            IWebDriver webDriver = new ChromeDriver();
+            webDriver.Manage().Window.Minimize();
+
+            Thread.Sleep(waitingTime);
+        }
+    }
+}
