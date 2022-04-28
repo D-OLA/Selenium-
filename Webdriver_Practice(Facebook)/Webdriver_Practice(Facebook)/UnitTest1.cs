b778d0db6385a38595489f496d7a8cda4e31579b
David6299 <dheveed3@gmail.com>
2022-04-28 13:15:04 +0000

    commit

--- Webdriver_Practice(Facebook)/Webdriver_Practice(Facebook)/UnitTest1.cs
+++ Webdriver_Practice(Facebook)/Webdriver_Practice(Facebook)/UnitTest1.cs
@@ -15,6 +15,9 @@ namespace Webdriver_Practice_Facebook_
             By googleSearchBar = By.Name("q");
             By googleSearchButton = By.Name("btnK");
             By facebookLink = By.XPath("/html/body/div[7]/div/div[10]/div/div[2]/div[2]/div/div/div[1]/div/div/div/div/div/div[1]/a/h3");
+            By emailOrPhoneNumber = By.XPath("/html/body/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div[1]/form/div[1]/div[1]/input");
+            By password = By.XPath("/html/body/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div[1]/form/div[1]/div[2]/div");
+            By login = By.XPath("/html/body/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div[1]/form/div[2]/button");
             
             IWebDriver webDriver = new ChromeDriver();
             webDriver.Manage().Window.Maximize();
@@ -34,6 +37,18 @@ namespace Webdriver_Practice_Facebook_
             Thread.Sleep(waitingTime);
 
             webDriver.FindElement(facebookLink).Click();
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.FindElement(emailOrPhoneNumber).SendKeys("08138924429");
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.FindElement(password).SendKeys("#Laitan99");
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.FindElement(login).Click();
 
             //webDriver.Quit();
         }
