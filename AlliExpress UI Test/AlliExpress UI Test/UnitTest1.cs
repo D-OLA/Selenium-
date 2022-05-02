3428113e805f30a91a80160ee3528cd7dc777880
David6299 <dheveed3@gmail.com>
2022-05-02 00:53:41 +0000

    commit

--- AlliExpress UI Test/AlliExpress UI Test/UnitTest1.cs
+++ AlliExpress UI Test/AlliExpress UI Test/UnitTest1.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using System;
+using System.Threading;
 
 namespace AlliExpress_UI_Test
 {
@@ -9,10 +9,38 @@ namespace AlliExpress_UI_Test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestAliExpressUI()
         {
+            int waitingTime = 1500;
+            By googleSearchBar = By.Name("q");
+            By googleSearchButton = By.Name("btnK");
+            By aliexpressLink = By.XPath("/html/body/div[7]/div/div[10]/div/div[1]/div[3]/div/div[1]/div/div/div/div[1]/a/div[1]/span");
+
             IWebDriver webDriver = new ChromeDriver();
-            webDriver.Navigate().GoToUrl("");
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.Manage().Window.Maximize();
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.Navigate().GoToUrl("https://www.google.com/");
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.FindElement(googleSearchBar).SendKeys("AliExpress login");
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.FindElement(googleSearchButton).Click();
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.FindElement(aliexpressLink).Click();
+
+            Thread.Sleep(waitingTime);
+
+            //webDriver.Quit();
         }
     }
 }
