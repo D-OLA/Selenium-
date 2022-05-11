80578d1450168bdf1b516f9dddeadbaed2da1558
David6299 <dheveed3@gmail.com>
2022-05-11 13:10:52 +0100

    commit

--- Alibaba(T)/Alibaba(T)/UnitTest1.cs
+++ Alibaba(T)/Alibaba(T)/UnitTest1.cs
@@ -12,9 +12,9 @@ namespace AlliExpress_UI_Test
         public void TestAliExpressUiAndAddItemsToCart()
         {
             int waitingTime = 1500;
-            By googleSearchBar = By.Name("q");
-            By googleSearchButton = By.Name("btnK");
-            By aliexpressLink = By.ClassName("LC201b MBeuO DKV0Md");
+            By googleSearchBar = By.XPath("/html[1]/body[1]/div[1]/div[3]/form[1]/div[1]/div[1]/div[1]/div[1]/div[2]/input[1]");
+            By googleSearchButton = By.Name("Btnk");
+            By aliexpressLink = By.XPath("/html[1]/body[1]/div[7]/div[1]/div[10]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[1]/div[1]/span[1]");
             By wishLink = By.XPath("/html/body/div[2]/div[3]/div/div[2]/div[5]/div[2]/a/span");
 
             IWebDriver webDriver = new ChromeDriver();
