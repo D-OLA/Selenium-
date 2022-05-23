aaac3cd10082c79563bebccf0c58a101fc374320
David6299 <dheveed3@gmail.com>
2022-05-23 09:11:02 +0100

    commit

---
+++ UseFast.com/UseFast.com/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace UseFast.com
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void OpenFast()
+        {
+            int Time = 1500;
+            IWebDriver webDriver = new ChromeDriver();
+            webDriver.Manage().Window.Maximize();
+            
+        }
+    }
+}
