675c595a4376e5f87410577225d23cb0afb3e23d
David6299 <dheveed3@gmail.com>
2022-05-26 12:03:48 +0100

    commit

---
+++ UseFastDotCom/UseFastDotCom/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UseFastDotCom")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UseFastDotCom")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d2e46c1e-d807-44be-80fb-1f926f20fafc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UseFastDotCom/UseFastDotCom/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace UseFastDotCom
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int waitingTime = 1500;
+            By searchBar = By.Name("q");
+            By googleSearchButton = By.Name("btnK");
+
+            IWebDriver webDriver = new ChromeDriver();
+            webDriver.Manage().Window.Maximize();
+
+            Thread.Sleep(waitingTime);
+            webDriver.Navigate().GoToUrl("https://google.com");
+            Thread.Sleep(waitingTime);
+            webDriver.FindElement(searchBar).SendKeys("www.fast.com");
+            Thread.Sleep(waitingTime);
+            webDriver.FindElement(googleSearchButton).Click();
+        }
+    }
+}
