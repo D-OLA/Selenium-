b3cd400059188d463fb6175ec5f316146a9b30b1
David6299 <dheveed3@gmail.com>
2022-05-31 00:56:58 +0100

    comit

---
+++ checkInternetSpeed/checkInternetSpeed/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("checkInternetSpeed")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("checkInternetSpeed")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a70431c8-8728-4bbb-95a6-2be407099b73")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ checkInternetSpeed/checkInternetSpeed/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace checkInternetSpeed
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void checkInternetSpeed()
+        {
+            int waitingTime = 8500;
+            By searchBar = By.Name("q");
+            By googleSearchButton = By.Name("btnK");
+
+            IWebDriver webDriver = new ChromeDriver();
+            Thread.Sleep(waitingTime);
+            webDriver.Manage().Window.Maximize();
+            Thread.Sleep(waitingTime);
+
+            webDriver.Navigate().GoToUrl("https://google.com");
+            Thread.Sleep(waitingTime);
+            webDriver.FindElement(searchBar).SendKeys("www.fast.com");
+            Thread.Sleep(waitingTime);
+            webDriver.FindElement(googleSearchButton).Click();
+
+            webDriver.Quit();
+        }
+    }
+}
