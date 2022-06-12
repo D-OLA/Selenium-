541cefa3974c1540947e4d36230a9b5abdfbfe4a
David6299 <dheveed3@gmail.com>
2022-06-12 23:17:13 +0100

    commit

--- checkInternetSpeed/checkInternetSpeed/UnitTest1.cs
+++ checkInternetSpeed/checkInternetSpeed/UnitTest1.cs
@@ -19,6 +19,10 @@ namespace checkInternetSpeed
             By refresh = By.XPath("/html[1]/body[1]/div[1]/div[2]/div[1]/div[3]/div[2]/div[1]/span[1]");
             By moreInfo = By.XPath("/html[1]/body[1]/div[1]/div[2]/div[1]/div[4]/div[1]/a[1]");
             By resultText1 = By.XPath("/html[1]/body[1]/div[1]/div[2]/div[1]/div[4]/div[2]/div[2]/div[1]/span[2]");
+            By settings = By.XPath("/html[1]/body[1]/div[1]/div[2]/div[1]/div[4]/div[2]/table[1]/tbody[1]/tr[1]/td[1]/a[1]/span[2]");
+            By minDur = By.XPath("/html[1]/body[1]/div[1]/div[2]/div[1]/div[4]/div[3]/div[2]/input[1]");
+            By maxDur = By.XPath("/html[1]/body[1]/div[1]/div[2]/div[1]/div[4]/div[3]/div[2]/input[2]");
+            By saveChanges = By.XPath("/html[1]/body[1]/div[1]/div[2]/div[1]/div[4]/div[3]/div[4]/a[2]");
 
             IWebDriver webDriver = new ChromeDriver();
             Thread.Sleep(waitingTime);
@@ -43,12 +47,22 @@ namespace checkInternetSpeed
             webDriver.FindElement(moreInfo).Click();
             
             var actualResult = webDriver.FindElement(resultText1);
-            Assert.IsTrue(actualResult.Text.Equals("Lagos, NG"));
+            Assert.IsTrue(actualResult.Text.Equals("Ibadan, NG"));
 
+            var actualResult2 = webDriver.FindElement(settings);
+            Assert.IsTrue(actualResult.Text.Equals("Settings"));
 
             Thread.Sleep(waitingTime);
+            webDriver.FindElement(settings).Click();
 
             Thread.Sleep(waitingTime);
+            webDriver.FindElement(minDur).SendKeys("5");
+
+            Thread.Sleep(waitingTime);
+            webDriver.FindElement(maxDur).SendKeys("15");
+
+            Thread.Sleep(waitingTime);
+            webDriver.FindElement(saveChanges).Click();
 
 
 
