c6679057817988eda7c5158aeeb940e260aee7bf
David6299 <dheveed3@gmail.com>
2022-07-16 20:39:10 +0100

    new test

---
+++ SteamUnlockedGamesCheck/SteamUnlockedGamesCheck/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SteamUnlockedGamesCheck")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SteamUnlockedGamesCheck")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9fc1605a-d0c6-4a2b-8af7-c1053ec3e016")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SteamUnlockedGamesCheck/SteamUnlockedGamesCheck/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace SteamUnlockedGamesCheck
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int waitTime = 1500;
+            By searchBar = By.Name("q");
+            By googleSearchButton = By.Name("btnK");
+            By SteamLink = By.XPath("/html[1]/body[1]/div[7]/div[1]/div[11]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[1]/h3[1]");
+
+            IWebDriver webDriver = new ChromeDriver();
+            Thread.Sleep(waitTime);
+            webDriver.Manage().Window.Maximize();
+
+            webDriver.Navigate().GoToUrl("https://www.google.com/");
+            Thread.Sleep(waitTime);
+
+            webDriver.FindElement(searchBar).SendKeys("SteamUnlocked.com");
+            webDriver.FindElement(googleSearchButton).Click();
+            Thread.Sleep(waitTime);
+
+            webDriver.FindElement(SteamLink).Click();
+            Thread.Sleep(waitTime);
+
+
+
+
+            webDriver.Close();
+        }
+
+        public void TestMethod2()
+        {
+        }
+
+        public void TestMethod3()
+        {
+        }
+
+
+    }
+}
