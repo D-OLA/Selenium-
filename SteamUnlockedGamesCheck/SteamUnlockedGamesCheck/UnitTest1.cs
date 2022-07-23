056028dbca4980fbb858aea743fde55f39c97c6d
David6299 <dheveed3@gmail.com>
2022-07-23 07:37:58 +0100

    commit

--- SteamUnlockedGamesCheck/SteamUnlockedGamesCheck/UnitTest1.cs
+++ SteamUnlockedGamesCheck/SteamUnlockedGamesCheck/UnitTest1.cs
@@ -13,8 +13,9 @@ namespace SteamUnlockedGamesCheck
         {
             int waitTime = 1500;
             By searchBar = By.Name("q");
-            By googleSearchButton = By.Name("btnK");
-            By SteamLink = By.XPath("/html[1]/body[1]/div[7]/div[1]/div[11]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[1]/h3[1]");
+            By googleSearchButton = By.XPath("/html[1]/body[1]/div[1]/div[3]/form[1]/div[1]/div[1]/div[3]/center[1]/input[1]");
+            By SteamLink = By.XPath("/html[1]/body[1]/div[7]/div[1]/div[10]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[1]/h3[1]");
+            By SteamUnlockedSearch = By.XPath("/html[1]/body[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[1]/input[1]");
 
             IWebDriver webDriver = new ChromeDriver();
             Thread.Sleep(waitTime);
@@ -30,7 +31,8 @@ namespace SteamUnlockedGamesCheck
             webDriver.FindElement(SteamLink).Click();
             Thread.Sleep(waitTime);
 
-
+            webDriver.FindElement(SteamUnlockedSearch).SendKeys("FIFA 22");
+            webDriver.FindElement(SteamUnlockedSearch).SendKeys(Keys.Enter);
 
 
             webDriver.Close();
