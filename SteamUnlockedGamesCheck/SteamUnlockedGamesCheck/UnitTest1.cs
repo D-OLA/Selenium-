c5235ea056d85fd018b8925c573fdf518229e65d
David6299 <dheveed3@gmail.com>
2022-08-26 03:57:25 +0100

    changes

--- SteamUnlockedGamesCheck/SteamUnlockedGamesCheck/UnitTest1.cs
+++ SteamUnlockedGamesCheck/SteamUnlockedGamesCheck/UnitTest1.cs
@@ -12,9 +12,10 @@ namespace SteamUnlockedGamesCheck
         public void TestMethod1()
         {
             int waitTime = 1500;
+            int waitTime2 = 18500;
             By searchBar = By.Name("q");
             By googleSearchButton = By.XPath("/html[1]/body[1]/div[1]/div[3]/form[1]/div[1]/div[1]/div[3]/center[1]/input[1]");
-            By SteamLink = By.XPath("/html[1]/body[1]/div[7]/div[1]/div[10]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[1]/h3[1]");
+            By SteamLink = By.XPath("/html[1]/body[1]/div[7]/div[1]/div[11]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[1]/h3[1]");
             By SteamUnlockedSearch = By.XPath("/html[1]/body[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[1]/input[1]");
 
             IWebDriver webDriver = new ChromeDriver();
@@ -29,22 +30,25 @@ namespace SteamUnlockedGamesCheck
             Thread.Sleep(waitTime);
 
             webDriver.FindElement(SteamLink).Click();
-            Thread.Sleep(waitTime);
+            Thread.Sleep(waitTime2);
 
             webDriver.FindElement(SteamUnlockedSearch).SendKeys("FIFA 22");
             webDriver.FindElement(SteamUnlockedSearch).SendKeys(Keys.Enter);
+            Thread.Sleep(waitTime2);
+
+            webDriver.FindElement(SteamUnlockedSearch).SendKeys("cyberpunk");
+            webDriver.FindElement(SteamUnlockedSearch).SendKeys(Keys.Enter);
+            Thread.Sleep(waitTime2);
+
+            webDriver.FindElement(SteamUnlockedSearch).SendKeys("WWE");
+            webDriver.FindElement(SteamUnlockedSearch).SendKeys(Keys.Enter);
+            Thread.Sleep(waitTime2);
 
 
             webDriver.Close();
         }
 
-        public void TestMethod2()
-        {
-        }
-
-        public void TestMethod3()
-        {
-        }
+        
 
 
     }
